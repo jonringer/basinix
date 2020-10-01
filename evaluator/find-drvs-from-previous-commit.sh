00921e8b0185d92e2f1bb13e22ed36ebfd3d5579
Jonathan Ringer <jonringer117@gmail.com>
2020-09-30 23:48:07 -0700

    Initial take on generating changed drvs

---
+++ evaluator/find-drvs-from-previous-commit.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash -p git nix jq
+
+set -euo pipefail
+
+echoerr() {
+        echo -e $@ >&2
+}
+
+usage() {
+        local name=$(basename $0)
+        echoerr "$name Create derivations from a given commit"
+        echoerr ""
+        echoerr "USAGE:"
+        echoerr "\t$name <commit-ish>"
+        echoerr ""
+        echoerr "ARGS:"
+        echoerr "\tcommit-ish>\t\t  git commit-ish from nixpkgs repo"
+        echoerr ""
+        echoerr "EXAMPLE:"
+        echoerr "\t$ $name abcdefg"
+        exit 1
+}
+
+[ "$1" == "--help" -o "$1" = "-h" ] && usage
+
+# validate environment
+if [ -z "${NIXPKGS_PATH:-}" ]; then
+        echoerr "error: Please set NIXPKGS_PATH before running script"
+        echoerr ""
+        usage
+fi
+
+if [ -z "${1:-}" ]; then
+        echoerr "No rev was given, please give a valid rev"
+        echoerr ""
+        usage
+fi
+rev="$1"
+
+#if test $(git cat-file --path="$NIXPKGS_PATH" -t commit) != commit; then
+#        echoerr "The rev \'$rev\' was not a valid commit"
+#        echoerr ""
+#        usage
+#fi
+
+set -x
+
+# create workdir
+worktreedir=$(mktemp -d)
+
+# remove tempdir, incase something fails
+#trap "rm -rf $worktreedir" EXIT
+pushd "$NIXPKGS_PATH"
+
+git worktree add $worktreedir $rev
+
+cd $worktreedir
+
+list_drvs() {
+        nix-env -I nixpkgs="$NIXPKGS_PATH" -f $worktreedir -qaP --no-name --drv-path --show-trace | sort
+}
+
+list_drvs > new_drvs.txt
+
+git checkout HEAD^
+
+list_drvs > old_drvs.txt
+
+comm -23 old_drvs.txt new_drvs.txt > diff_drvs.txt
+
