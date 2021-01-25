51ae7a538b25cb9d9139a814a19a1fa3978bf28f
Jonathan Ringer <jonringer117@gmail.com>
2021-01-24 16:59:44 -0800

    Include github polling thread

--- server/src/main.rs
+++ server/src/main.rs
@@ -11,34 +11,37 @@ use basinix_shared::eval_events::EvalRequest;
 use chrono::Local;
 use std::io::Write;
 
+const LOG_TARGET: &str = "basinix::server::main";
+
 #[tokio::main]
 async fn main() {
     env_logger::Builder::from_default_env()
         .format(|buf, record| {
             writeln!(buf,
                 "[{}] {:5} - {} - {}",
-                Local::now().format("%Y-%m-%d %H:%M:%S %Z"),
+                Local::now().to_rfc3339(),
                 record.level(),
                 record.target(),
                 record.args()
             )
         }).init();
 
-    // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let hello = warp::path!("hello" / String)
-        .map(|name| format!("Hello, {}!", name));
-
     let (tx, rx) = mpsc::channel::<EvalRequest>();
-    error!("Starting github polling thread");
-    let handle = thread::spawn(move|| {
+    info!(target: LOG_TARGET, "Starting github polling thread");
+    thread::Builder::new().name("github_producer".to_string()).spawn(move|| {
         produce_github_pr_events(tx)
     });
+
     println!("waiting to receive");
     println!("{:?}", rx.recv().unwrap());
     let sleep_time = Duration::from_secs(20);
     thread::sleep(sleep_time);
 
     let stream = UnixStream::connect("/run/user/1000/basinix/basinix.sock").unwrap();
+
+    // GET /hello/warp => 200 OK with body "Hello, warp!"
+    let hello = warp::path!("hello" / String)
+        .map(|name| format!("Hello, {}!", name));
 
     warp::serve(hello)
         .run(([127, 0, 0, 1], 3030))
